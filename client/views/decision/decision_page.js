15e6e88298716befe3ca2ec867b1c097ed6e56c0
Zoe Madden-Wood <maddenwood@gmail.com>
2014-07-26 17:29:09 -0700

    Adds apathy suggestions in addition to your decision.

--- client/views/decision/decision_page.js
+++ client/views/decision/decision_page.js
@@ -4,5 +4,28 @@ Template.DecisionPage.helpers({
     var maybe = this.could_dos[Math.floor(Math.random()*this.could_dos.length)];
     console.log("maybe", maybe, this.could_dos);
     return maybe.maybe;
+  },
+
+  apathy: function() {
+    var apathy = apathyData[Math.floor(Math.random()*apathyData.length)];
+    return apathy.apathyMessage;
   }
 });
+
+var apathyData = [ 
+  {
+    apathyMessage: "... or just stay home and watch Game of Thrones."
+  },
+  {
+    apathyMessage: "... or just sleep now, avoid the rush."
+  },
+  {
+    apathyMessage: "Remember, there's a lot do out there... why don't you sleep?"
+  },
+  {
+    apathyMessage: "... or you could put on pajamas and call it a day."
+  },
+  {
+    apathyMessage: "... or you could just watch Orange is the New Black."
+  }
+]
