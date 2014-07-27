83d0dfdc053949bf945aed36fcb72c78f90b5445
Zoe Madden-Wood <maddenwood@gmail.com>
2014-07-26 17:34:42 -0700

    More apathy messages.

--- client/views/decision/decision_page.js
+++ client/views/decision/decision_page.js
@@ -16,16 +16,28 @@ var apathyData = [
   {
     apathyMessage: "... or just stay home and watch Game of Thrones."
   },
+
   {
     apathyMessage: "... or just sleep now, avoid the rush."
   },
+
   {
     apathyMessage: "Remember, there's a lot do out there... why don't you sleep?"
   },
+
   {
     apathyMessage: "... or you could put on pajamas and call it a day."
   },
+
   {
     apathyMessage: "... or you could just watch Orange is the New Black."
+  }, 
+
+  {
+  	apathyMessage: "... but face it, you don't have the energy to do that right now anyway. If you did, you wouldn't be on this app."
+  },
+
+  {
+  	apathyMessage: "... or you could just google baby kittens."
   }
 ]
