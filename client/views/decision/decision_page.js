859ed1cdec10b0927817e4be908b8e1ec873c9ba
Zoe Madden-Wood <maddenwood@gmail.com>
2014-07-26 17:00:47 -0700

    Changes button name.

--- client/views/decision/decision_page.js
+++ client/views/decision/decision_page.js
@@ -2,6 +2,7 @@ Template.DecisionPage.helpers({
   maybe: function() {
   	console.log("in maybe");
     var maybe = this.could_dos[Math.floor(Math.random()*this.could_dos.length)];
+    console.log("maybe", maybe, this.could_dos);
     return maybe.maybe;
   }
 });
